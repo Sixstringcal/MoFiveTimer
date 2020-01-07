cf86c227046f1886d689079b4e4447616790deb2
Calvin Nielson <sixstringcal@gmail.com>
2020-01-07 12:25:57 -0700

    Enter submits time.  Added labels to display time.  Time properly gets read in without formatting punctuation and assumes punctuation.

--- src/Timer.java
+++ src/Timer.java
@@ -17,25 +17,37 @@ public class Timer extends JFrame {
     private JComboBox eventListBox;
     private JTextField enterTimeField;
     private JLabel scrambleLabel;
+    private JLabel ao5Time1;
+    private JLabel ao5Time2;
+    private JLabel ao5Time3;
+    private JLabel ao5Time4;
+    private JLabel ao5Time5;
     private JScrollPane scrambleAreaScrollable;
     private JTextPane scramblePane;
     private String currentScramble;
     private String scrambleType;
-    private double[] mo5 = new double[5];
+    private int firstIndex;
+    private JLabel[] ao5Labels = {ao5Time1, ao5Time2, ao5Time3, ao5Time4, ao5Time5};
+    private double[] ao5 = new double[5];
     private String[] eventNames = {"Square-1", "3x3", "2x2", "4x4", "5x5", "6x6", "7x7", "Skewb", "Pyraminx", "Megaminx"};
 
     public Timer() {
         add(mainPanel);
+        for(JLabel temp : ao5Labels){
+            temp.setText("");
+        }
         setBackground(Color.DARK_GRAY);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         mainPanel.setBackground(Color.DARK_GRAY);
         setSize(900, 900);
         enterTimeField.setBackground(Color.DARK_GRAY);
-
+        firstIndex = 0;
         setVisible(true);
-        scrambleType = "Square-1";
+        scrambleType = "3x3";
         generateScramble();
         eventListBox.setModel(new DefaultComboBoxModel(eventNames));
+        eventListBox.setSelectedIndex(1);
+
         eventListBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -48,8 +60,23 @@ public class Timer extends JFrame {
         enterTimeField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == 13) {
-
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    String rawInput = enterTimeField.getText();
+                    try {
+                        ao5[firstIndex] = Double.parseDouble(rawInput) / 100;
+                        int errorFromMinutes = (int) Math.floor(ao5[firstIndex] / 100);
+                        ao5[firstIndex] = ao5[firstIndex] - errorFromMinutes * 40;
+                        ao5Labels[firstIndex].setText("" + ao5[firstIndex]);
+                        firstIndex = (firstIndex) % 5;
+                        enterTimeField.setText("");
+                        if (firstIndex == 0) {
+                            for (JLabel temp : ao5Labels) {
+                                temp.setText("");
+                            }
+                        }
+                    } catch(Exception exception) {
+                        
+                    }
                 }
             }
         });
@@ -100,7 +127,7 @@ public class Timer extends JFrame {
         for (int i = 0; i < charredScramble.length; i++) {
             displayedScramble += charredScramble[i];
             if (j % screenWidth == 0) {
-                if (i < charredScramble.length-1) {
+                if (i < charredScramble.length - 1) {
                     if (charredScramble[i] != 'w' && charredScramble[i + 1] != '\'' && charredScramble[i + 1] != 'w' && (charredScramble[i] != '3' && !scrambleType.equals("Square-1"))) {
                         displayedScramble += "<br>";
                     } else {
